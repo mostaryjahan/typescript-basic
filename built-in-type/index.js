6bde2bff57d51b7ee9058f7971649be1b6886327
Mostary Jahan <mostaryjahan01@gmail.com>
2024-09-21 11:18:36 +0600

    learn built in type

---
+++ built-in-type/index.js
@@ -0,0 +1,8 @@
+//built in type:  number, string, boolean, void, undefined, null
+var userId;
+var userName;
+var isActive;
+userId = 101;
+userName = "Bristy";
+isActive = true;
+console.log(userId);
---
+++ built-in-type/index.ts
@@ -0,0 +1,12 @@
+//built in type:  number, string, boolean, void, undefined, null
+
+
+let userId: number;
+let userName: string;
+let isActive: boolean;
+
+userId = 101;
+userName = "Bristy";
+isActive = true;
+
+console.log(userId);
